3065bbde74c6719b3a80e2cfc20579510370b384
Lokiii27 <lokesh.srimalla@gmail.com>
2025-02-21 16:10:38 +0530

    Update exJava.java

--- exJava.java
+++ exJava.java
@@ -1,6 +1,7 @@
 public class exJava{
     public static void main(String[] args) {
         System.out.println("Hello from exJava");
-        System.out.println("Hello2 from exJava");
+        System.out.println("Hello3 from exJava");
     }
 }
+3
